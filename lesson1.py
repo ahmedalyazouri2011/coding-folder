386a90ee9843c7b4815de9ffbc355540ffd3fcdb
ahmedalyazouri2011 <alyazouriahmed393@gmail.com>
2026-07-23 23:37:58 +0300

    add homework2

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+print("welcome to the world of coding")
+print("4")
+print("hello python\nhello python")
+print("hello",5)
+x=5
+y="ahmed"
+print(x)
+print(y)
+name=input("enter your name ")
+print(name)
