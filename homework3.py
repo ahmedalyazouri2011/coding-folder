5f81e1e2208a5c4a537002492e461958dc87b40e
ahmedalyazouri2011 <alyazouriahmed393@gmail.com>
2026-08-02 15:04:10 +0300

    Add homework3.py

---
+++ homework3.py
@@ -0,0 +1,14 @@
+num1 = int(input(" Enter first number: "))
+num2 = int(input(" Enter second number: "))
+
+sum = num1 + num2
+difference = num1 - num2
+
+sum += 5 
+
+print(" Sum after adding 5:", sum)
+print("difference:", difference)
+
+print(" Is the first number greater than the second?", num1 > num2)
+print(" Are the numbers equal?", num1 == num2)
+print(" Are the numbers different?", num1 != num2)
