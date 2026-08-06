ebdca793c476b96813c40ec372239b15cb08e05f
ahmedalyazouri2011 <alyazouriahmed393@gmail.com>
2026-08-06 20:38:42 +0300

    add homework5.py

---
+++ homework5.py
@@ -0,0 +1,10 @@
+print("Summer Temperature Checker")
+
+temp = int(input(" Enter the temperature: "))
+
+if temp > 35:
+    print(" It is very hot today ")
+elif temp >= 25:
+    print(" It is a nice summer day ")
+else:
+    print(" It is cool today ")
