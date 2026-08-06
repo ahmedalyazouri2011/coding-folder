672cee073d07eb7212e96c1e31d247fd289deeea
ahmedalyazouri2011 <alyazouriahmed393@gmail.com>
2026-08-06 20:48:39 +0300

    add homework6.py

---
+++ homework6.py
@@ -0,0 +1,7 @@
+print("Checking Alphabets")
+character = input(" Enter a character: ")
+
+if (character >= 'A' and character <= 'Z') or (character >= 'a' and character <= 'z'):
+ print("it is an alphabet")
+else:
+ print(" it is not an alphabet")
