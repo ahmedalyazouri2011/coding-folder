179e09a140b81962b3251de581374711aa15768f
ahmedalyazouri2011 <alyazouriahmed393@gmail.com>
2026-08-07 14:28:34 +0300

    add homework7.py

---
+++ homework7.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+
+if age >= 10:
+    if age <= 20:
+        print("Your age is between 10 and 20.")
+    else:
+        print("Your age is not bewtween 10 and 20.")
+else:
+    print("Your age is not between 10 and 20.")
+    
